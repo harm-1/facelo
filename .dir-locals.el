41f66bb6250243d0ae64cb6fde1e57ee44dc2e32
Harm <hveekeren.d@gmail.com>
2023-10-10 20:30:00 +0200

    add dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((python-mode . ((pytest-global-name . "docker-compose run --rm backend pytest")
+                 (projectile-project-test-cmd . "docker-compose run --rm backend pytest backend/tests/"))))
